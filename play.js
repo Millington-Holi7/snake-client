f64869b89c0930f554adb34bc234672ecbf0471d
Millington-Holi7 <holinas007@gmail.com>
2024-02-21 17:30:02 -0500

    moved the connection logic to its own file and module.export to still use it.

--- play.js
+++ play.js
@@ -1,19 +1,6 @@
-const net = require("net");
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  });
+const {connect} = require("./client")
 
-  conn.on("data", (data) => {
-    console.log("server says: ", data);
-  });
-
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("connecting  . . .");
 connect();
