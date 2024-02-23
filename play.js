6feed359376ddcb2c6c65f0b33b2c1c74178f3b6
Millington-Holi7 <holinas007@gmail.com>
2024-02-22 20:07:29 -0500

    added file input to place all of the users input.

--- play.js
+++ play.js
@@ -1,26 +1,8 @@
-const {connect} = require("./client")
+//const { connect } = require("./client")
+const { setupInput } = require("./input")
 console.log("connecting  . . .");
-const conn = connect();
+//connect()
 
-const setupInput = function(key) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume(); 
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  // Send movement commands to the server based on key pressed.
-  if (key === '\x1b[A') conn.write(`Move: up`);
-  if (key === '\x1b[C') conn.write(`Move: right`);
-  if (key === '\x1b[B') conn.write(`Move: down`);
-  if (key === '\x1b[D') conn.write(`Move: left`);
-};
 
 
 setupInput();
